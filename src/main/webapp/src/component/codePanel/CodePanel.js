52872e737d46e9372cfe6129d95b725138a70561
Nikolaichenko <nikolaichenkod@gmail.com>
2020-07-07 01:41:09 +0300

    [VADSH-31] Graph code dynamically fetched from backend and displayed on GUI

--- src/main/webapp/src/component/codePanel/CodePanel.js
+++ src/main/webapp/src/component/codePanel/CodePanel.js
@@ -9,29 +9,60 @@ import '../css/CodePanel.css';
 
 class CodePanel extends React.Component {
 
-    downloadGraphFile = () => {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        this.fetchGraphCode();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (JSON.stringify(prevProps) !== JSON.stringify(this.props)) {
+            this.fetchGraphCode();
+        }
+    }
+
+    fetchGraphCode = () => {
+        let self = this;
         fetch('/generator/graph', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(this.props.graph)
-        }).then(response =>
-            response.blob().then(blob => {
-                let url = window.URL.createObjectURL(blob);
-                let a = document.createElement('a');
-                a.href = url;
-                a.download = 'Graph.java';
-                a.click();
-            }));
+        }).then((response) => {
+            return response.text();
+        }).then((data) => {
+            self.setState({code: data});
+        });
     }
 
+    // downloadGraphFile = () => {
+    //     fetch('/generator/graph', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(this.props.graph)
+    //     }).then(response =>
+    //         response.blob().then(blob => {
+    //             let url = window.URL.createObjectURL(blob);
+    //             let a = document.createElement('a');
+    //             a.href = url;
+    //             a.download = 'Graph.java';
+    //             a.click();
+    //         }));
+    // }
+
     render() {
-        let src = "Hello, World!";
+        console.log("Rendering code panel");
+
         return (
             <div className="App-code-panel">
                 <UnControlled
-                    value={src}
+                    value={this.state.code}
                     options={{
                         lineNumbers: true,
                         mode: "text/x-java",
