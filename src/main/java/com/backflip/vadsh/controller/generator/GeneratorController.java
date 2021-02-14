f4da92017eea4857a336c287afa506249d519c1e
Nikolaichenko <nikolaichenkod@gmail.com>
2021-02-14 21:19:06 +0200

    [VADSH-105] Generator controller created

---
+++ src/main/java/com/backflip/vadsh/controller/generator/GeneratorController.java
@@ -0,0 +1,38 @@
+package com.backflip.vadsh.controller.generator;
+
+import com.backflip.vadsh.ds.graph.generator.GeneratorOption;
+import com.backflip.vadsh.ds.graph.generator.GraphGenerator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Log4j2
+@Controller
+@RequestMapping("/generator")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class GeneratorController {
+
+    @ResponseBody
+    @GetMapping(path = "options", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public OptionsResponse availableOptions(@RequestParam(name = "for", defaultValue = "") List<GeneratorOption> forOptions) {
+        return OptionsResponse.builder().availableOptions(GeneratorOption.compatibleOptionsFor(forOptions)).build();
+    }
+
+    @ResponseBody
+    @GetMapping(path = "generate", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public GraphResponse generateGraph(@RequestParam(name = "for") List<GeneratorOption> forOptions) {
+        GraphGenerator generator = GraphGenerator.getGenerator(ThreadLocalRandom.current().nextInt(3, 15), forOptions);
+        return GraphResponse.builder()
+                .graph(generator.getGraph().adjacencyMatrix())
+                .config(generator.getConfig())
+                .build();
+    }
+
+}
---
+++ src/test/java/com/backflip/vadsh/controller/generator/GeneratorControllerTest.java
@@ -0,0 +1,55 @@
+package com.backflip.vadsh.controller.generator;
+
+import com.backflip.vadsh.ds.graph.generator.GeneratorOption;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.collection.IsIterableWithSize.iterableWithSize;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GeneratorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getAllGeneratorOptions() throws Exception {
+        mockMvc.perform(get("/generator/options")
+                .contentType(APPLICATION_JSON))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.availableOptions", iterableWithSize(GeneratorOption.allOptions().size())));
+    }
+
+    @Test
+    public void getGeneratorOptions() throws Exception {
+        mockMvc.perform(get("/generator/options")
+                .contentType(APPLICATION_JSON)
+                .queryParam("for", "WEIGHTED, DIRECTED"))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.availableOptions", not(empty())));
+    }
+
+    @Test
+    public void generate() throws Exception {
+        mockMvc.perform(get("/generator/generate")
+                .contentType(APPLICATION_JSON)
+                .queryParam("for", "WEIGHTED, DIRECTED, COMPLETE"))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.graph", not(empty())))
+                .andExpect(jsonPath("$.config", not(empty())));
+    }
+}
