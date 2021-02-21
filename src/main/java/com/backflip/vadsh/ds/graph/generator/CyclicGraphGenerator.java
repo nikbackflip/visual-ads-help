6d20e8016f0b3364aa15303abf0b8cfb451a24b7
Nikolaichenko <nikolaichenkod@gmail.com>
2021-02-21 10:20:17 +0200

    [VADSH-103] Cyclic sparse graph generator implemented

--- src/main/java/com/backflip/vadsh/ds/graph/generator/CyclicGraphGenerator.java
+++ src/main/java/com/backflip/vadsh/ds/graph/generator/CyclicGraphGenerator.java
@@ -1,7 +1,14 @@
 package com.backflip.vadsh.ds.graph.generator;
 
 import com.backflip.vadsh.ds.graph.Config;
+import com.backflip.vadsh.ds.graph.Edge;
 import com.backflip.vadsh.ds.graph.Graph;
+
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static com.backflip.vadsh.ds.graph.Edge.edge;
+import static com.backflip.vadsh.ds.graph.generator.GeneratorOption.*;
 
 public class CyclicGraphGenerator implements GraphGenerator {
 
@@ -9,6 +16,8 @@ public class CyclicGraphGenerator implements GraphGenerator {
     private final GeneratorOption weight;
     private final GeneratorOption direction;
     private final GeneratorOption density;
+
+    private final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     protected CyclicGraphGenerator(int size, GeneratorOption weight, GeneratorOption direction, GeneratorOption density) {
         this.size = size;
@@ -19,12 +28,108 @@ public class CyclicGraphGenerator implements GraphGenerator {
 
     @Override
     public Graph getGraph() {
-        return null;
+        LinkedList<Edge> edges = new LinkedList<>();
+        int cycleSize = calculateCycleSize();
+
+        boolean[] connected = new boolean[size];
+        for (int i = 0; i < cycleSize; i++) {
+            connected[i] = true;
+        }
+        shuffleArray(connected);
+
+        int[] cycle = new int[cycleSize];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (connected[i]) {
+                cycle[j] = i;
+                j++;
+            }
+        }
+
+        if (cycle.length == 0) {
+            addEdge(edges, 0, 0);
+        } else {
+            int prevNode = cycle[0];
+            for (int i = 1; i < cycleSize; i++) {
+                addEdge(edges, prevNode, cycle[i]);
+                prevNode = cycle[i];
+            }
+            addEdge(edges, prevNode, cycle[0]);
+        }
+
+        return new Graph(edges, size);
+    }
+
+    private void addEdge(LinkedList<Edge> edges, int from, int to) {
+        double calcWeight = calculateWeight();
+        edges.add(edge(from, to, calcWeight));
+        if (direction == NOT_DIRECTED) {
+            edges.add(edge(to, from, calcWeight));
+        }
     }
 
     @Override
     public Config getConfig() {
-        return null;
+        return new Config(
+                direction == GeneratorOption.DIRECTED,
+                weight == GeneratorOption.WEIGHTED,
+                true);
+    }
+
+    private int minCycle() {
+        if (size < 3) return 1;
+        int maxEdges = calculateMaxEdges();
+        if (density == SPARSE && maxEdges <= 3) return 1;
+        return 3;
+    }
+
+    private int maxCycle() {
+        int maxEdges = calculateMaxEdges();
+        if (size == 1) return 1;
+        if (size == 2) return direction == DIRECTED ? 2 : 1;
+        if (density == SPARSE && maxEdges <= size) return maxEdges;
+        return size;
+    }
+
+    private int calculateCycleSize() {
+        return minCycle() == maxCycle() ? maxCycle() :
+                random.nextInt(minCycle(), maxCycle());
+    }
+
+
+    private double calculateWeight() {
+        return weight == NOT_WEIGHTED ? 1.0 : random.nextInt(1, 20);
+    }
+
+    private int calculateMaxEdges() {
+        int temp = (density == SPARSE) ? (int)Math.floor((size * size) * 0.5) : size * size;
+        return direction == DIRECTED ? temp : (int)Math.floor(temp / 2);
+    }
+
+    private double calculateDensity() {
+        if (density == DENSE)  return random.nextDouble(0.6, 0.8);
+        return random.nextDouble(0.1, 0.3);
+    }
+
+    private int calculateExpectedEdges(int maxEdges, double density) {
+        if (this.density == DENSE) return (int) Math.ceil(maxEdges * density);
+        else return (int) Math.floor(maxEdges * density);
+    }
+
+    //Fisher–Yates
+    private void shuffleArray(boolean[] array) {
+        int index;
+        for (int i = array.length - 1; i > 0; i--) {
+            index = random.nextInt(i + 1);
+            if (index != i) {
+                array[index] ^= array[i];
+                array[i] ^= array[index];
+                array[index] ^= array[i];
+            }
+        }
+    }
+    public static void main(String[] args) {
+        new CyclicGraphGenerator(3, NOT_WEIGHTED, NOT_DIRECTED, SPARSE).getGraph();
     }
 
 }
---
+++ src/test/java/com/backflip/vadsh/ds/graph/generator/CyclicGeneratorTest.java
@@ -0,0 +1,88 @@
+package com.backflip.vadsh.ds.graph.generator;
+
+import com.backflip.vadsh.ds.graph.Config;
+import com.backflip.vadsh.ds.graph.Graph;
+import com.backflip.vadsh.ds.graph.analyzer.AnalyticDefinition;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.backflip.vadsh.ds.graph.generator.GeneratorOption.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CyclicGeneratorTest {
+
+    @ParameterizedTest
+    @MethodSource("input")
+    public void taskTest(int size, GeneratorOption weight, GeneratorOption direction, GeneratorOption density,
+                         List<AnalyticDefinition> expectedAnalytics, List<AnalyticDefinition> notExpectedAnalytics) {
+        //given
+        GraphGenerator generator = new CyclicGraphGenerator(size, weight, direction, density);
+
+        //when
+        Graph graph = generator.getGraph();
+        Config config = generator.getConfig();
+
+        //then
+        assertEquals(size, graph.n());
+        expectedAnalytics.forEach(analytic -> assertTrue(analytic.analyze(graph, config)));
+        notExpectedAnalytics.forEach(analytic -> assertFalse(analytic.analyze(graph, config)));
+    }
+
+    private static Stream<Arguments> input() {
+        return Stream.of(
+                Arguments.of(
+                        1, NOT_WEIGHTED, DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.DIRECTIONAL, AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED)),
+                Arguments.of(
+                        2, NOT_WEIGHTED, DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.DIRECTIONAL, AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED)),
+                Arguments.of(
+                        3, NOT_WEIGHTED, DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.DIRECTIONAL, AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED)),
+                Arguments.of(
+                        5, NOT_WEIGHTED, DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.DIRECTIONAL, AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED)),
+                Arguments.of(
+                        10, NOT_WEIGHTED, DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.DIRECTIONAL, AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED)),
+                Arguments.of(
+                        15, NOT_WEIGHTED, DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.DIRECTIONAL, AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED)),
+                Arguments.of(
+                        1, NOT_WEIGHTED, NOT_DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED, AnalyticDefinition.DIRECTIONAL)),
+                Arguments.of(
+                        2, NOT_WEIGHTED, NOT_DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED, AnalyticDefinition.DIRECTIONAL)),
+                Arguments.of(
+                        3, NOT_WEIGHTED, NOT_DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED, AnalyticDefinition.DIRECTIONAL)),
+                Arguments.of(
+                        5, NOT_WEIGHTED, NOT_DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED, AnalyticDefinition.DIRECTIONAL)),
+                Arguments.of(
+                        10, NOT_WEIGHTED, NOT_DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED, AnalyticDefinition.DIRECTIONAL)),
+                Arguments.of(
+                        15, NOT_WEIGHTED, NOT_DIRECTED, DENSE,
+                        List.of(AnalyticDefinition.CYCLIC),
+                        List.of(AnalyticDefinition.WEIGHTED, AnalyticDefinition.DIRECTIONAL))
+        );
+    }
+
+}
