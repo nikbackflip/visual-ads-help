771f08998570cc5713828fbf5e47c13391dcd03d
Nikolaichenko <nikolaichenkod@gmail.com>
2021-03-07 18:19:06 +0200

    [VADSH-122] Eulerian path analyzer fixed to detect disconnected components, find connected components task addded

--- src/test/java/com/backflip/vadsh/ds/graph/task/FindConnectedComponentsTest.java
+++ src/test/java/com/backflip/vadsh/ds/graph/task/FindConnectedComponentsTest.java
@@ -21,13 +21,13 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.isA;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FindStronglyConnectedComponentsTest {
+public class FindConnectedComponentsTest {
 
     @ParameterizedTest
     @MethodSource("input")
     public void taskTest(Graph graph, List<Set<Integer>> expected) {
         //given
-        Task task = new FindStronglyConnectedComponents();
+        Task task = new FindConnectedComponents();
 
         //when
         TaskResult result = task.execute(graph, directionalConfig, Collections.emptyMap());
@@ -41,11 +41,9 @@ public class FindStronglyConnectedComponentsTest {
     private final static Graph oneNodeGraph = new Graph(List.of(), 1);
     private final static Graph twoNodesGraph = new Graph(List.of(), 2);
     private final static Graph triangleGraph = new Graph(List.of(new Edge(0, 1, 1.0), new Edge(1, 0, 1.0), new Edge(1, 2, 1.0), new Edge(2, 1, 1.0), new Edge(2, 0, 1.0), new Edge(0, 2, 1.0)), 3);
-    private final static Graph twoComponentsGraph = new Graph(List.of(new Edge(0, 1, 1.0),new Edge(1, 2, 1.0),new Edge(2, 0, 1.0),new Edge(0, 3, 1.0),new Edge(3, 3, 1.0),new Edge(3, 4, 1.0),new Edge(4, 3, 1.0),new Edge(4, 5, 1.0),new Edge(5, 6, 1.0),new Edge(6, 4, 1.0)), 7);
-    private final static Graph threeComponents = new Graph(List.of(new Edge(0, 1, 1.0), new Edge(1, 2, 1.0), new Edge(2, 3, 1.0), new Edge(3, 2, 1.0), new Edge(3, 4, 1.0), new Edge(4, 5, 1.0), new Edge(5, 4, 1.0), new Edge(1, 6, 1.0), new Edge(6, 0, 1.0)), 7);
-    private final static Graph fourComponentsGraph = new Graph(List.of(new Edge(0, 1, 1.0), new Edge(1, 0, 1.0), new Edge(0, 8, 1.0), new Edge(8, 0, 1.0), new Edge(8, 7, 1.0), new Edge(7, 6, 1.0), new Edge(6, 7, 1.0), new Edge(1, 7, 1.0), new Edge(2, 1, 1.0), new Edge(2, 6, 1.0), new Edge(5, 6, 1.0), new Edge(2, 5, 1.0), new Edge(5, 3, 1.0), new Edge(3, 2, 1.0), new Edge(4, 3, 1.0), new Edge(4, 5, 1.0)), 9);
-    private final static Graph twoDisjointComponentsGraph = new Graph(List.of(new Edge(0, 1, 1.0), new Edge(1, 0, 1.0), new Edge(2, 3, 1.0), new Edge(3, 4, 1.0), new Edge(4, 2, 1.0)), 5);
-    private final static Graph butterflyGraph = new Graph(List.of(new Edge(0, 1, 1.0), new Edge(1, 2, 1.0), new Edge(2, 3, 1.0), new Edge(3, 1, 1.0), new Edge(1, 4, 1.0), new Edge(4, 0, 1.0)), 5);
+    private final static Graph disconnectedGraph = new Graph(List.of(new Edge(1, 2, 1.0), new Edge(2, 1, 1.0), new Edge(2, 4, 1.0), new Edge(4, 2, 1.0), new Edge(4, 1, 1.0), new Edge(1, 4, 1.0), new Edge(0, 3, 1.0), new Edge(3, 0, 1.0)), 5);
+    private final static Graph disconnectedUndirectedGraph = new Graph(List.of(new Edge(3, 5, 1.0), new Edge(5, 3, 1.0), new Edge(5, 4, 1.0), new Edge(4, 5, 1.0), new Edge(4, 6, 1.0), new Edge(6, 4, 1.0), new Edge(6, 3, 1.0), new Edge(3, 6, 1.0), new Edge(2, 3, 1.0), new Edge(3, 2, 1.0), new Edge(7, 8, 1.0), new Edge(8, 7, 1.0), new Edge(10, 10, 1.0), new Edge(0, 9, 1.0), new Edge(9, 0, 1.0)), 11);
+    private final static Graph disconnectedDirectedGraph = new Graph(List.of(new Edge(1, 0, 1.0), new Edge(2, 3, 1.0), new Edge(3, 9, 1.0), new Edge(9, 2, 1.0), new Edge(5, 9, 1.0), new Edge(4, 4, 1.0), new Edge(7, 7, 1.0), new Edge(8, 8, 1.0), new Edge(8, 7, 1.0), new Edge(6, 2, 1.0)), 10);
 
     private static Stream<Arguments> input() {
         return Stream.of(
@@ -53,11 +51,9 @@ public class FindStronglyConnectedComponentsTest {
                 Arguments.of(oneNodeGraph, List.of(Set.of(0))),
                 Arguments.of(twoNodesGraph, List.of(Set.of(0), Set.of(1))),
                 Arguments.of(triangleGraph, List.of(Set.of(0, 1, 2))),
-                Arguments.of(twoComponentsGraph, List.of(Set.of(0, 1, 2), Set.of(3, 4, 5, 6))),
-                Arguments.of(threeComponents, List.of(Set.of(0, 1, 6), Set.of(2, 3), Set.of(4, 5))),
-                Arguments.of(twoDisjointComponentsGraph, List.of(Set.of(0, 1), Set.of(2, 3, 4))),
-                Arguments.of(fourComponentsGraph, List.of(Set.of(0, 1, 8), Set.of(6, 7), Set.of(2, 3, 5), Set.of(4))),
-                Arguments.of(butterflyGraph, List.of(Set.of(0, 1, 2, 3, 4)))
+                Arguments.of(disconnectedGraph, List.of(Set.of(0, 3), Set.of(1, 2, 4))),
+                Arguments.of(disconnectedUndirectedGraph, List.of(Set.of(0, 9), Set.of(1), Set.of(2, 3, 4, 5, 6), Set.of(7, 8), Set.of(10))),
+                Arguments.of(disconnectedDirectedGraph, List.of(Set.of(0, 1), Set.of(4), Set.of(2, 3, 5, 6, 9), Set.of(7, 8)))
         );
     }
 
@@ -69,7 +65,7 @@ public class FindStronglyConnectedComponentsTest {
         Config config = new Config(true, true, true);
 
         //when
-        TaskResult result = TaskDefinition.FIND_STRONGLY_CONNECTED_COMPONENTS.execute(graph, config, params);
+        TaskResult result = TaskDefinition.FIND_CONNECTED_COMPONENTS.execute(graph, config, params);
 
         //then
         assertThat(result, isA(TaskExecutionSuccess.class));
@@ -84,28 +80,12 @@ public class FindStronglyConnectedComponentsTest {
         Config config = new Config(true, true, true);
 
         //when
-        TaskResult result = TaskDefinition.FIND_STRONGLY_CONNECTED_COMPONENTS.execute(graph, config, params);
+        TaskResult result = TaskDefinition.FIND_CONNECTED_COMPONENTS.execute(graph, config, params);
 
         //then
         assertThat(result, isA(TaskExecutionFailed.class));
         assertTrue(result.failed());
         assertEquals("Wrong parameters number", ((TaskExecutionFailed)result).getMessage());
-    }
-
-    @Test
-    public void validatePrerequisites_failOnUndirectedGraph() {
-        //given
-        Map<String, Integer> params = emptyMap();
-        Graph graph = new Graph(List.of(new Edge(1, 2, 1.0),new Edge(2, 1, 1.0)), 2);
-        Config config = new Config(false, true, true);
-
-        //when
-        TaskResult result = TaskDefinition.FIND_STRONGLY_CONNECTED_COMPONENTS.execute(graph, config, params);
-
-        //then
-        assertThat(result, isA(TaskExecutionFailed.class));
-        assertTrue(result.failed());
-        assertEquals("Graph must be directional", ((TaskExecutionFailed)result).getMessage());
     }
 }
 
